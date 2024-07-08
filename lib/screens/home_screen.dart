b07254e84b7f7986d6b0b6f1aec78faeca36c030
ROHANUR RAHMAN <mdrohanurrahmanontu@gmail.com>
2024-07-09 02:56:35 +0600

    late initilizer related issue solved on the home screen and sign out feature added

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:letstest/screens/quiz_create.dart';
+import 'package:letstest/screens/sign_in.dart';
+import 'package:letstest/services/authentication.dart';
 import 'package:letstest/services/database.dart';
 import '../utils/const.dart';
 
@@ -14,6 +16,7 @@ class _HomeScreenState extends State<HomeScreen> {
   final DatabaseService databaseService = DatabaseService();
   Stream getQuizes = Stream.empty();
   bool hasError = false;
+  AuthService authService = AuthService();
 
   @override
   void initState() {
@@ -40,19 +43,27 @@ class _HomeScreenState extends State<HomeScreen> {
           style: TextStyle(fontSize: 18.0, color: kCommonColor),
         ),
         centerTitle: true,
+        leading: IconButton(
+          icon: Icon(
+            Icons.add,
+            color: kCommonColor,
+          ),
+          onPressed: () {
+            Navigator.pushReplacement(
+                context, MaterialPageRoute(builder: (_) => QuizCreate()));
+          },
+        ),
         actions: [
           IconButton(
             onPressed: () {
-              Navigator.pushReplacement(
-                context,
-                MaterialPageRoute(builder: (_) => const QuizCreate()),
-              );
+              authService.signOut();
+              Navigator.pushReplacement(context, MaterialPageRoute(builder: (_)=>SignIn()));
             },
             icon: const Icon(
-              Icons.add,
+              Icons.logout,
               color: kCommonColor,
             ),
-          )
+          ),
         ],
         automaticallyImplyLeading: false,
         elevation: 0,
@@ -61,49 +72,49 @@ class _HomeScreenState extends State<HomeScreen> {
         padding: const EdgeInsets.all(16.0),
         child: hasError
             ? const Center(
-          child: Text(
-            'Failed to load quizzes. Please try again later.',
-            style: TextStyle(color: kCommonColor),
-          ),
-        )
-            : StreamBuilder(
-          stream: getQuizes,
-          builder: (context, AsyncSnapshot snapshot) {
-            if (snapshot.connectionState == ConnectionState.waiting) {
-              return const Center(
-                child: CircularProgressIndicator(
-                  color: kCommonColor,
-                ),
-              );
-            }
-            if (!snapshot.hasData || snapshot.data.docs.isEmpty) {
-              return const Center(
                 child: Text(
-                  'No quizzes available.',
+                  'Failed to load quizzes. Please try again later.',
                   style: TextStyle(color: kCommonColor),
                 ),
-              );
-            }
-            var quizDocs = snapshot.data.docs;
-            return ListView.builder(
-              itemCount: quizDocs.length,
-              itemBuilder: (context, index) {
-                var quizData = quizDocs[index].data();
-                return Card(
-                  elevation: 4,
-                  child: ListTile(
-                    title: Text(quizData['quizTitle']),
-                    subtitle: Text(quizData['quizDesc']),
-                    leading: Image.network(
-                      quizData['quizImgUrl'],
-                      fit: BoxFit.cover,
-                    ),
-                  ),
-                );
-              },
-            );
-          },
-        ),
+              )
+            : StreamBuilder(
+                stream: getQuizes,
+                builder: (context, AsyncSnapshot snapshot) {
+                  if (snapshot.connectionState == ConnectionState.waiting) {
+                    return const Center(
+                      child: CircularProgressIndicator(
+                        color: kCommonColor,
+                      ),
+                    );
+                  }
+                  if (!snapshot.hasData || snapshot.data.docs.isEmpty) {
+                    return const Center(
+                      child: Text(
+                        'No quizzes available.',
+                        style: TextStyle(color: kCommonColor),
+                      ),
+                    );
+                  }
+                  var quizDocs = snapshot.data.docs;
+                  return ListView.builder(
+                    itemCount: quizDocs.length,
+                    itemBuilder: (context, index) {
+                      var quizData = quizDocs[index].data();
+                      return Card(
+                        elevation: 4,
+                        child: ListTile(
+                          title: Text(quizData['quizTitle']),
+                          subtitle: Text(quizData['quizDesc']),
+                          leading: Image.network(
+                            quizData['quizImgUrl'],
+                            fit: BoxFit.cover,
+                          ),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
       ),
     );
   }
